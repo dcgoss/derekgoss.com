9ef3e4a44d3df6b0bbc34f640beaeb66a44f0950
dcgoss <dcgoss@Dereks-MacBook-Pro.local>
2014-05-27 22:53:16 -0400

    changed fill in

--- fillin.js
+++ fillin.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 });
 
 function adjectives(i) {
-	var words = ["plays lacrosse.", "made this website from scratch.", "is a sauce aficionado.", "ran a 5:30 mile.", "is fluent in Python.", "loves sailing.", "always has a new product idea.", "wants to found a startup.", "is learning Java.", "plays percussion.", "is moving to Philadelphia.", "is developing mobile applications.", "runs cross-country."];
+	var words = ["plays lacrosse.", "made this website from scratch.", "is a sauce aficionado.", "ran a 5:30 mile.", "is fluent in Python.", "loves sailing.", "always has a new product idea.", "wants to found a startup.", "is learning Java.", "plays percussion.", "is moving to Philadelphia.", "always wears bowties.", "runs cross-country."];
 
 		$("#fillin").fadeOut(500, function() {
 			$(this).text(words[i]).fadeIn(500);
