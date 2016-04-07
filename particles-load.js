427eb4942cb407913e67eee287fcdd951e9b0008
dcgoss <dcgoss14@gmail.com>
2016-04-07 00:05:27 -0400

    Added particles.js and SASS
    Right now the particles just fill the screen.

---
+++ particles-load.js
@@ -0,0 +1,7 @@
+/**
+ * Created by dcgoss on 4/5/16.
+ */
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles-config.json', function() {
+    console.log('callback - particles.js config loaded');
+});
