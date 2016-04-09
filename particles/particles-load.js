d4f8a7aa3da8f65bf2861b6d3f25e4623d079414
dcgoss <dcgoss14@gmail.com>
2016-04-09 00:27:34 -0400

    Reorganized files into folders; added CSS for link buttons
    Forgot to reorganize beforehand so I had to combine the two commits.

--- particles/particles-load.js
+++ particles/particles-load.js
@@ -2,6 +2,6 @@
  * Created by dcgoss on 4/5/16.
  */
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particles-config.json', function() {
+particlesJS.load('particles-js', 'particles/particles-config.json', function() {
     console.log('callback - particles.js config loaded');
 });
